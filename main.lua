574dcfe0a2d22418bfcea9b5d53990afae082c6b
GoldKing <hjhj117@hotmail.com>
2019-05-14 22:28:16 +0800

    Feat: supportSSL

--- main.lua
+++ main.lua
@@ -1,6 +1,11 @@
 local GHEMailManager = require "lib.GHEMailManager"
 
-local EMailManager = GHEMailManager:init('imap.163.com', 143)
+local EMailManager = GHEMailManager:init('imap.163.com', 993, {protocol = 'tlsv1_2'})
+
 EMailManager:login("gkebook@163.com", "xxxxxx")
 EMailManager:loadAllMail()
 EMailManager:logout()
+
+-- require("ssl")
+-- local https = require("ssl.https")
+-- sudo luarocks install LuaSec
